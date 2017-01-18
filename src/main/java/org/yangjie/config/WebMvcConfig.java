503ad3d0e90ed74293d9c6e4fda30e90f4684cb1
yangjie <yangjie@moko.cc>
2017-01-18 19:05:25 +0800

    修改页面等

--- src/main/java/org/yangjie/config/WebMvcConfig.java
+++ src/main/java/org/yangjie/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package org.yangjie.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * web mvc 配置类
  * @author YangJie [2015年11月4日 下午1:30:08]
  */
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Override // 默认首页
